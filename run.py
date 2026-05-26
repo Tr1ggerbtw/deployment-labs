c670bdce00cc39638052a852310ed7082bc90e2b
Leheza Daniil <lehesadanylo27@gmail.com>
2026-05-26 04:27:14 +0300

    feat: change run.py and docker-compose.yml so it works for all labs

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
+import os
 from app import create_app
 from app.config import config
+from app.db import db
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    app.run(
-        host=config['server']['host'],
-        port=config['server']['port']
-    )
+    run_host = os.getenv('APP_HOST', config['server']['host'])
+    run_port = int(os.getenv('APP_PORT', config['server']['port']))
+    
+    app.run(host=run_host, port=run_port)
